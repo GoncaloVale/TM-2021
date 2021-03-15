ca6bcb14697cde8264460dc51a7ad653bff19d43
Gonçalo Vale <goncalovale@ipvc.pt>
2021-03-15 18:26:12 +0000

    Aula4 GonçaloVale-23472

--- Aula 4/Rename with Name-Number/scripts/script.js
+++ Aula 4/Rename with Name-Number/scripts/script.js
@@ -58,20 +58,20 @@ function create() {
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
+        frames: [{key: 'dude', frame: 4}],
         frameRate: 20
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
         frameRate: 10,
         repeat: -1
     });
@@ -81,7 +81,7 @@ function create() {
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
+        setXY: {x: 12, y: 0, stepX: 70}
     });
 
     stars.children.iterate(function (child) {
@@ -92,15 +92,14 @@ function create() {
 
     this.physics.add.collider(stars, platforms);
     this.physics.add.overlap(player, stars, collectStar, null, this);
-    function collectStar (player, star)
-    {
+
+    function collectStar(player, star) {
         star.disableBody(true, true);
 
         score += 10;
         scoreText.setText('Score: ' + score);
 
-        if (stars.countActive(true) === 0)
-        {
+        if (stars.countActive(true) === 0) {
             stars.children.iterate(function (child) {
 
                 child.enableBody(true, child.x, 0, true, true);
@@ -119,7 +118,7 @@ function create() {
 
     var score = 0;
     var scoreText;
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText = this.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#000'});
 
     bombs = this.physics.add.group();
 
@@ -127,8 +126,7 @@ function create() {
 
     this.physics.add.collider(player, bombs, hitBomb, null, this);
 
-    function hitBomb (player, bomb)
-    {
+    function hitBomb(player, bomb) {
         this.physics.pause();
 
         player.setTint(0xff0000);
@@ -145,27 +143,21 @@ function update() {
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown) {
         player.setVelocityX(-160);
 
         player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
+    } else if (cursors.right.isDown) {
         player.setVelocityX(160);
 
         player.anims.play('right', true);
-    }
-    else
-    {
+    } else {
         player.setVelocityX(0);
 
         player.anims.play('turn');
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
-    {
+    if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
 
